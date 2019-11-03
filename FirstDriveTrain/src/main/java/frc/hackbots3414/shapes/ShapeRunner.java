949ac55b6da74cd08f13702f6518cfc473e85cd5
Mr. Z <bczdog@gmail.com>
2019-11-03 10:05:42 -0500

    Added the Oval example, with a little help from the Internet

--- FirstDriveTrain/src/main/java/frc/hackbots3414/shapes/ShapeRunner.java
+++ FirstDriveTrain/src/main/java/frc/hackbots3414/shapes/ShapeRunner.java
@@ -31,7 +31,13 @@ public class ShapeRunner {
     public static ArrayList<Shape> createShapeList() {
         ArrayList<Shape> shapes = new ArrayList<>();
         // TODO create all the shapes and add them to the list
+
+        shapes.add(createOval());
         return shapes;
+    }
+
+    private static Shape createOval() {
+        return new Oval(15.75, 12.25);
     }
 
     /**
